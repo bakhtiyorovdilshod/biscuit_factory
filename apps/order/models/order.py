cd78fd48bdddb4bf2e7201c270049cad6bacf44d
Dilshod Bakhtiyorov <bakhtiyorovdilshod440@gamil.com>
2021-01-31 22:43:50 +0500

    fix

--- apps/order/models/order.py
+++ apps/order/models/order.py
@@ -15,7 +15,7 @@ class ClientOrder(models.Model):
     quantity = models.DecimalField(max_digits=20, decimal_places=2, default=decimal.Decimal(0))
     comment = models.TextField()
     status = models.CharField(max_length=50, choices=status_type, default='pending', blank=True, null=True)
-    created_date = models.DateField(auto_now_add=True, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     modified_date = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
